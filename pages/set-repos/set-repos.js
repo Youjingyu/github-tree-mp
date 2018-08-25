9d7416d362758f465e1601f0e5aa4db2bfebc14b
Youjingyu <735284268@qq.com>
2018-08-25 13:57:06 +0800

    feat: 根据粘贴板自动填充项目地址

--- pages/set-repos/set-repos.js
+++ pages/set-repos/set-repos.js
@@ -2,6 +2,19 @@ Page({
   data: {
     inputValue: ''
   },
+  onReady () {
+    const that = this
+    wx.getClipboardData({
+      success (res) {
+        const repos = parseGithubUrl(res.data)
+        if (repos) {
+          that.setData({
+            inputValue: repos
+          })
+        }
+      }
+    })
+  },
   bindKeyInput (e) {
     this.setData({
       inputValue: e.detail.value
@@ -11,17 +24,18 @@ Page({
     const val = this.data.inputValue
     if (val === '') return
     if (/^http(s)?:\/\//.test(val)) {
-      if (!/^https:\/\/github\.com\/[^/]+?\/[^/]+?[/]{0,1}$/.test(val)) {
+      const repos = parseGithubUrl(val)
+      if (!repos) {
         wx.showModal({
           title: '提示',
           content: '请输入正确的github项目地址',
           showCancel: false
         })
-      } else {
-        wx.navigateTo({
-          url: '/pages/index/index?repos=' + val
-        })
+        return
       }
+      wx.navigateTo({
+        url: '/pages/index/index?repos=' + val
+      })
     } else {
       wx.navigateTo({
         url: '/pages/search-res/search-res?query=' + val
@@ -29,3 +43,9 @@ Page({
     }
   }
 })
+
+function parseGithubUrl (text) {
+  if (!/^https:\/\//.test(text)) return null
+  const matches = text.match(/(https:\/\/github\.com\/[^/]+?\/[^/]+[/]{0,1})/)
+  return matches && matches[1]
+}
