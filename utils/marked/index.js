f0737a7b4ea7e3555b978bc08a65fa0027a32a8e
Youjingyu <735284268@qq.com>
2018-09-06 20:36:58 +0800

    reset：还原到markdown优化之前

---
+++ utils/marked/index.js
@@ -0,0 +1,53 @@
+const marked = require('marked')
+const htmlParser = require('../wxParse/htmlparser')
+const Prism = require('../prism/prism')
+const supportLanguage = require('../prism/language').supportLanguage
+
+const renderer = new marked.Renderer()
+
+renderer.html = function (html) {
+  let res = ''
+  htmlParser(html, {
+    start (tagName, attrs, unary) {
+      res += '<' + tagName
+      for (var i = 0; i < attrs.length; i++) {
+        res += ' ' + attrs[i].name + '="' + attrs[i].escaped + '"'
+      }
+      res += '>'
+    },
+    end (tag) {
+      res += '</' + tag + '>'
+    },
+    chars (text) {
+      res += text
+    }
+  })
+  return res
+}
+renderer.code = function (code, type, escaped) {
+  let html
+  const render = supportLanguage[type]
+  if (render) {
+    html = Prism.highlight(code, Prism.languages[render], render)
+  } else {
+    html = escapedHtml(code)
+  }
+  html = html.replace(/\n/g, '<br>')
+  return `<div style="background-color: #f6f8fa;padding: 10px;margin: 10px 0;">${html}</div>`
+}
+
+function escapedHtml (html) {
+  return html
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;')
+}
+
+module.exports = function (md) {
+  return marked(md, {
+    renderer,
+    headerIds: false
+  })
+}
---
+++ utils/prism/language.js
@@ -0,0 +1,35 @@
+const map = {
+  'js': { lang: 'javascript', render: 'javascript' },
+  'css': { lang: 'css', render: 'css' },
+  'wxss': { lang: 'wxss', render: 'css' },
+  'html': { lang: 'html', render: 'markup' },
+  'wxml': { lang: 'wxml', render: 'markup' },
+  'vue': { lang: 'vue', render: 'markup' },
+  'jsx': { lang: 'jsx', render: 'markup' },
+  'tsx': { lang: 'typescript', render: 'typescript' },
+  'ts': { lang: 'typescript', render: 'typescript' },
+  'json': { lang: 'json', render: 'json' },
+  'dart': { lang: 'dart', render: 'dart' },
+  'go': { lang: 'go', render: 'go' },
+  'less': { lang: 'less', render: 'less' },
+  'scss': { lang: 'sass', render: 'sass' },
+  'java': { lang: 'java', render: 'java' },
+  'py': { lang: 'py', render: 'python' },
+  'php': { lang: 'php', render: 'php' },
+  'kt': { lang: 'kotlin', render: 'kotlin' },
+  'swift': { lang: 'swift', render: 'swift' },
+  'c': { lang: 'c', render: 'c' },
+  'h': { lang: 'h', render: 'h' },
+  'm': { lang: 'm', render: 'm' },
+  'cpp': { lang: 'c++', render: 'clike' },
+  'cs': { lang: 'c#', render: 'clike' },
+  'sh': { lang: 'bash', render: 'bash' }
+}
+const supportLanguage = Object.keys(map).reduce((obj, key) => {
+  obj[map[key].lang] = map[key].render
+  return obj
+}, {})
+module.exports = {
+  languageMap: map,
+  supportLanguage
+}
