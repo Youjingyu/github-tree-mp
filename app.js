aeef73d8fd19221faef043ce66845efd50e4790c
Youjingyu <735284268@qq.com>
2018-09-10 23:09:59 +0800

    feat: 添加版本更新机制

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const hightlight = require('./utils/prism/index.js')
 
 App({
   onLaunch: function () {
+    const updateManager = wx.getUpdateManager()
+    updateManager.onCheckForUpdate(function (res) {
+      // 请求完新版本信息的回调
+      console.log('has new version: ' + res.hasUpdate)
+    })
   },
   onError (error) {
     wx.showToast({
